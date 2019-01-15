4a1390633225c7dac79d7e7008ebe8de2b966620
khalidnt <405795+khalidnt@users.noreply.github.com>
2019-01-15 08:36:57 +0300

    punchchuation seed file

--- db/seed.sql
+++ db/seed.sql
@@ -23,7 +23,7 @@ CREATE TABLE saved_job
   FOREIGN KEY (user_id) REFERENCES users ON DELETE CASCADE
 );
 
-INSERT INTO users (username, email, phone) VALUES ('aisha',"a@a.com", 0550123456 );
-INSERT INTO users (username, email, phone) VALUES ('nada',"n@n.com", 0550654321 );
-INSERT INTO users (username, email, phone) VALUES ('nawaf',"n2@n.com", 0550123123 );
-INSERT INTO users (username, email, phone) VALUES ('khaled',"k@k.com", 0550321321 );
+INSERT INTO users (username, email, phone) VALUES ('aisha','a@a.com', '0550123456' );
+INSERT INTO users (username, email, phone) VALUES ('nada', 'n@n.com', '0550654321' );
+INSERT INTO users (username, email, phone) VALUES ('nawaf', 'n2@n.com', '0550123123' );
+INSERT INTO users (username, email, phone) VALUES ('khaled','k@k.com', '0550321321' );
