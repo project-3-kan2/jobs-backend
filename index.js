1b5bce957e67913023fa02bf2ed67e2945bf1906
khalidnt <405795+khalidnt@users.noreply.github.com>
2019-01-20 09:54:07 +0300

    redeploy heroku

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
-  res.send('Jobs, Jobs, Jobs');
+  res.send('Jobs, jobs, Jobs');
 })
 
 const usersController = require('./controllers/usersControllers');
